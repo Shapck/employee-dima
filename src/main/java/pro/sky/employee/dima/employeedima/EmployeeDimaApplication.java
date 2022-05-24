f3dabf9a22705382d6625f78fc30ce6f0f40e543
Дима <dimonsoldatov@gmsil.com>
2022-05-24 18:58:17 +0300

    Initial commit

---
+++ src/main/java/pro/sky/employee/dima/employeedima/EmployeeDimaApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.employee.dima.employeedima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDimaApplication.class, args);
+	}
+
+}
